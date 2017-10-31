51685b9abf9b5be6083f22a8b037cacc07a28afe
shyamalajinju <shyamalajinju@gmail.com>
2017-10-31 23:44:54 +0530

    initial commit

---
+++ src/StringEx.java
@@ -0,0 +1,13 @@
+public class StringEx {
+
+	public static void main(String[] args) {
+			
+String str="hello world        ";
+//str.trim();
+System.out.println(str+str);
+System.out.println(str.trim()+str);
+String s1="hi      ";
+System.out.println(s1.trim()+s1);
+	}
+
+}
